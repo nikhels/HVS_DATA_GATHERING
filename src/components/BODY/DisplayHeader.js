4a45d53384082b007b3c92ba25c0e7bd10eebab8
Brant Nichols <nikhels@hotmail.com>
2020-12-30 14:44:42 -0600

    updated-note-subnet-fixed

--- src/components/BODY/DisplayHeader.js
+++ src/components/BODY/DisplayHeader.js
@@ -1,40 +1,31 @@
-import React,{ useContext } from 'react'
-import { GlobalContext } from '../App'
+import React, { useContext } from "react";
+import { GlobalContext } from "../App";
 
 export default function DisplayHeader() {
   const {
     equipmentTypeSelection,
     equipmentSelection,
     auxiliaryInformation,
+  } = useContext(GlobalContext);
 
-  } = useContext(GlobalContext)
-  
-  const {tsid,callLetters,} = auxiliaryInformation
+  const { tsid, callLetters } = auxiliaryInformation;
 
-
- 
   return (
-
     <>
-    
-    <div className="display__sheet-header-container">
-      <div className="display__sheet-header">{equipmentSelection} </div>
-      <div className="display__sheet-header">{callLetters} {equipmentTypeSelection} Parameters </div>
-      <div className="display__sheet-branding">
-        Heartland Video Systems <br />
-        1-800-332-7088
-      </div> 
-    
-    </div>
-    <div className="display__sheet-header-secondary">
-      <div className="display__sheet-sub-header">TSID: {tsid}</div>
-      <div></div>
-      
-      
-    </div>
-
-
-  
+      <div className="display__sheet-header-container">
+        <div className="display__sheet-header">{equipmentSelection} </div>
+        <div className="display__sheet-header">
+          {callLetters} {equipmentTypeSelection} Parameters{" "}
+        </div>
+        <div className="display__sheet-branding">
+          Heartland Video Systems <br />
+          1-800-332-7088
+        </div>
+      </div>
+      {auxiliaryInformation.tsid &&
+      <div className="display__sheet-header-secondary">
+        <div className="display__sheet-sub-header">TSID: {tsid}</div>
+      </div>}
     </>
-  )
+  );
 }
